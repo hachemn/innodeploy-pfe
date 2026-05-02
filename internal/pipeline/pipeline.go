a9ea5079e55973db3e163f22f373347d674583bd
hachemn <hachemnourhen8@gmail.com>
2026-05-02 12:41:06 +0100

    deploy v1777722054

--- internal/pipeline/pipeline.go
+++ internal/pipeline/pipeline.go
@@ -1,32 +1,59 @@
 package pipeline
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
+	"time"
 )
 
 type Step struct {
 	Name    string
 	Command string
 }
+
 func RunPipeline(repo string) {
 	log.Println("🚀 Pipeline started for:", repo)
+
+	// 🟢 version unique
+	version := fmt.Sprintf("v%d", time.Now().Unix())
+
+	imageName := "nourhenhachem/innodeploy-app:" + version
 
 	steps := []Step{
 		{"Cleanup", "rm -rf project"},
 		{"Clone", "git clone " + repo + " project"},
+
+		// build
 		{"Build Docker Image", "cd project && docker build -t innodeploy-app ."},
-		{"Tag Image", "docker tag innodeploy-app nourhenhachem/innodeploy-app:latest"},
-		{"Push Image", "docker push nourhenhachem/innodeploy-app:latest"},
-		{"Deploy to Kubernetes", "cd project && kubectl apply -f k8s/deployment.yaml"},
+
+		// tag avec version
+		{"Tag Image", "docker tag innodeploy-app " + imageName},
+
+		// push
+		{"Push Image", "docker push " + imageName},
+
+		// 🔥 update YAML (GitOps)
+		{"Update YAML", fmt.Sprintf(
+			"sed -i 's|image:.*|image: %s|' k8s/deployment.yaml",
+			imageName,
+		)},
+
+		// push git
+		{"Git Commit", fmt.Sprintf(
+			"git add . && git commit -m 'deploy %s'",
+			version,
+		)},
+		{"Git Push", "git push"},
 	}
 
 	for _, step := range steps {
 		runStep(step)
 	}
 
-	log.Println("✅ Pipeline finished")
+	log.Println("✅ Pipeline finished 🚀 version:", version)
 }
+
 func runStep(step Step) {
 	log.Println("➡️ Step:", step.Name)
 
